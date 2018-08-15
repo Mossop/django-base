c2c06566572386c716c5bddf8c40c9d31245b982
Dave Townsend <dtownsend@oxymoronical.com>
2018-08-14 20:52:27 -0700

    More updates for django 2.

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import include, url
+from django.urls import path, include
 from .utils import config
 
 from config.urls import urlpatterns
 
 if config.get("admin", "enabled") == "true":
     from django.contrib import admin
-    urlpatterns.append(url(r'^admin/', admin.site.urls))
+    urlpatterns.append(path('admin/', admin.site.urls))
