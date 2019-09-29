99f2bd8803e7a035b7725860a41f894f6f3d30e0
Dave Townsend <dtownsend@oxymoronical.com>
2019-09-28 19:20:28 -0700

    Add static serving.

--- urls.py
+++ urls.py
@@ -1,8 +1,17 @@
 from django.urls import path, include
 from .utils import config
+from .settings import DEBUG, STATIC_URL, STATIC_ROOT
 
-from config.urls import urlpatterns
+from config.urls import app_url_patterns
+
+urlpatterns = []
 
 if config.get("admin", "enabled") == "true":
     from django.contrib import admin
-    urlpatterns.append(path('admin/', admin.site.urls))
+    urlpatterns += path('admin/', admin.site.urls)
+
+if DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(STATIC_URL, document_root = STATIC_ROOT)
+
+urlpatterns.extend(app_url_patterns)
