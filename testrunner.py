3c07b6eb07a5460a146a629015eee81518baedf0
Dave Townsend <dtownsend@oxymoronical.com>
2020-04-12 22:38:44 -0700

    Only log errors from django during tests.

--- testrunner.py
+++ testrunner.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.test.runner import DiscoverRunner
 
 from .config import TEST_MODE
@@ -6,4 +8,7 @@ class TestSuiteRunner(DiscoverRunner):
     def __init__(self, *args, **kwargs):
         if not TEST_MODE:
             raise Exception('Unexpected test run outside of test mode.')
+
+        logger = logging.getLogger('django')
+        logger.setLevel(logging.ERROR)
         super(TestSuiteRunner, self).__init__(*args, **kwargs)
